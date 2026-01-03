10693b3d8547d867dae02dcf444e9ef9c588a1d1
Goodbadboy <selfmanademola@gmail.com>
2026-01-03 14:05:25 +0100

    feat: update voting program to include additional poll parameters
    - Modified voting.json to change "_poll_id" to "poll_id" and added "description", "poll_start", and "poll_end" fields.
    - Updated TypeScript types in voting.ts to reflect new parameters.
    - Refactored votingapp.test.ts to initialize a poll with the new parameters using the BankrunProvider.
    - Updated package-lock.json to remove unused dependencies.

--- anchor/tests/votingapp.test.ts
+++ anchor/tests/votingapp.test.ts
@@ -1,29 +1,75 @@
-import {
-  Blockhash,
-  createSolanaClient,
-  createTransaction,
-  generateKeyPairSigner,
-  Instruction,
-  isSolanaError,
-  KeyPairSigner,
-  signTransactionMessageWithSigners,
-} from 'gill'
-import {
-  fetchVotingapp,
-  getCloseInstruction,
-  getDecrementInstruction,
-  getIncrementInstruction,
-  getInitializeInstruction,
-  getSetInstruction,
-} from '../src'
-// @ts-ignore error TS2307 suggest setting `moduleResolution` but this is already configured
-import { loadKeypairSignerFromFile } from 'gill/node'
+import { PublicKey } from "@solana/web3.js";
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { BN, Program } from "@coral-xyz/anchor";
+import { Voting } from '../target/types/voting';
+import * as anchor from "@coral-xyz/anchor";
+import path from "path";
 
-const { rpc, sendAndConfirmTransaction } = createSolanaClient({ urlOrMoniker: process.env.ANCHOR_PROVIDER_URL! })
+const IDL = require("../target/idl/voting.json");
+
+const votingAddress = new PublicKey("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 
 describe('voting', () => {
-
   it('Initialize Poll', async () => {
+    // Use absolute path to anchor directory
+    const context = await startAnchor(
+      path.join(__dirname, ".."), 
+      [{ name: "voting", programId: votingAddress }],
+      []
+    );
 
-  })
-})
+    const provider = new BankrunProvider(context);
+
+    const votingProgram = new Program<Voting>(
+      IDL,
+      provider,
+    );
+
+    await votingProgram.methods
+      .initializePoll(
+        new anchor.BN(1),
+        "what is your favorite type of peanut butter",
+        new anchor.BN(0),
+        new anchor.BN(1967442516),
+      )
+      .rpc();
+
+    console.log("Poll initialized successfully!");
+  });
+});
+
+// import { PublicKey } from "@solana/web3.js";
+// import { BankrunProvider, startAnchor } from "anchor-bankrun";
+// import { BN, Program } from "@coral-xyz/anchor";
+// import * as anchor from "@coral-xyz/anchor";
+// import { Voting } from '../target/types/voting';
+
+// const IDL = require("../target/idl/voting.json");
+
+// const votingAddress = new PublicKey("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
+
+// describe('voting', () => {
+
+//   it('Initialize Poll', async () => {
+//     const context = await startAnchor(
+//       "./",
+//       [{ name: "voting", programId: votingAddress }],
+//       []
+//     );
+
+//     const provider = new BankrunProvider(context);
+
+//     const votingProgram = new Program<Voting>(
+//       IDL,
+//       provider,
+//     );
+
+//     await votingProgram.methods.initializePoll(
+//       new anchor.BN(1),
+//       "what is your favorite type of peanut butter",
+//       new anchor.BN(0),
+//       new anchor.BN(1967442516),
+//     ).rpc();
+
+//   });
+// });
