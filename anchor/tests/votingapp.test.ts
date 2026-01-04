8478ebcb630e6c411a3306620b7290b52363e711
Goodbadboy <selfmanademola@gmail.com>
2026-01-04 12:22:59 +0100

    Remove commented-out code and redundant test structure from votingapp.test.ts

--- anchor/tests/votingapp.test.ts
+++ anchor/tests/votingapp.test.ts
@@ -198,39 +198,3 @@ describe('voting', () => {
     }
   });
 });
-
-// import { PublicKey } from "@solana/web3.js";
-// import { BankrunProvider, startAnchor } from "anchor-bankrun";
-// import { BN, Program } from "@coral-xyz/anchor";
-// import * as anchor from "@coral-xyz/anchor";
-// import { Voting } from '../target/types/voting';
-
-// const IDL = require("../target/idl/voting.json");
-
-// const votingAddress = new PublicKey("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
-
-// describe('voting', () => {
-
-//   it('Initialize Poll', async () => {
-//     const context = await startAnchor(
-//       "./",
-//       [{ name: "voting", programId: votingAddress }],
-//       []
-//     );
-
-//     const provider = new BankrunProvider(context);
-
-//     const votingProgram = new Program<Voting>(
-//       IDL,
-//       provider,
-//     );
-
-//     await votingProgram.methods.initializePoll(
-//       new anchor.BN(1),
-//       "what is your favorite type of peanut butter",
-//       new anchor.BN(0),
-//       new anchor.BN(1967442516),
-//     ).rpc();
-
-//   });
-// });
