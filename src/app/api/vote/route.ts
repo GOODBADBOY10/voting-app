81b41106b25828d4c82d27bdbfe3b360619b9c0b
Goodbadboy <selfmanademola@gmail.com>
2026-01-05 06:44:27 +0100

    feat: implement GET API route to return voting metadata for peanut butter preferences

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,5 +1,13 @@
+import { ActionGetResponse } from "@solana/actions";
 
 
 export async function GET(request: Request) {
-    return new Response('Hello from API');
+    const actionMetadata: ActionGetResponse = {
+        icon: "https://media.gettyimages.com/id/75941645/photo/woman-scooping-peanut-butter-from-jar.jpg?s=612x612&w=0&k=20&c=etUc0P3NdZ6TdjKmTEURwQg74hTys7UL2FSjoFIzVYY=",
+        title: "Vote for your favorite type of peanut butter",
+        description: "vote between cruchy and smooth peanut butter",
+        label: "Vote",
+    }
+    return Response.json(actionMetadata);
+    // return new Response('Hello from API');
 }
