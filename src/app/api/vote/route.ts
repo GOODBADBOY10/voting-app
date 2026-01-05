ea98f34578e45a708ff29e09c05e741716ffa5b4
Goodbadboy <selfmanademola@gmail.com>
2026-01-05 06:58:36 +0100

    feat: add OPTIONS constant for handling preflight requests in voting API

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,6 +1,8 @@
 import { ActionGetResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
 
 
+export const OPTIONS = GET;
+
 export async function GET(request: Request) {
     const actionMetadata: ActionGetResponse = {
         icon: "https://media.gettyimages.com/id/75941645/photo/woman-scooping-peanut-butter-from-jar.jpg?s=612x612&w=0&k=20&c=etUc0P3NdZ6TdjKmTEURwQg74hTys7UL2FSjoFIzVYY=",
